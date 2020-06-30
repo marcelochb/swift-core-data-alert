1a24ba66d9ea8ce84d32a75fddfcd0a64ee6fbb2
Marcelo Block <marcelochb@gmail.com>
2020-06-30 06:32:30 -0300

    refact code

--- DemoArtigo/AlertClass.swift
+++ DemoArtigo/AlertClass.swift
@@ -6,11 +6,41 @@
 //  Copyright © 2020 Guilherme Paciulli. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class AlertClass {
     
-    func configAlert(firstTextInput: String, secondTextInput: String, thirdTextInput: String) {
+    func configAlert(firstTextInput: String, secondTextInput: String, thirdTextInput: String) -> UIAlertController {
+        let Alert = UIAlertController(title: "Adicionar vinho",
+        message: "Nome do vinho",
+        preferredStyle: .alert)
+        
+        let AlertCancelButton = NextAction(title: "Cancelar")
+        let AlertNextButton = NextAction(title: "Proximo")
+        
+        Alert.addTextField()
+            Alert.addAction(AlertCancelButton)
+            Alert.addAction(AlertNextButton)
+            
+            self.present(Alert, animated: true)
+        
+        return Alert
+    }
+    
+    func NextAction (title: String) -> UIAlertAction {
+        var AlertAction: UIAlertAction?
+        if (title == "Cancelar") {
+            AlertAction = UIAlertAction(title: title,
+            style: .default)
+        }
+        if (title == "Proximo") {
+            AlertAction = UIAlertAction(title: "Terminar", style: .default) { [unowned self] action in
+                
+                
+            }
+        }
+        
+        return AlertAction!
         
     }
 }
